2de104ebf820cc9091d0112b0e9fbf3123ac717c
Ivanildo Cândido <systechtecnologia@gmail.com>
2022-04-06 09:37:12 -0300

    Database CRUD

---
+++ database_connect/addUser.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<h2>Cadastro de Usuários</h2>
+<form method="POST" action="addUser_action.php">
+    <label>Digite um nome:</label>
+    <input type="text" name="name" />
+    <label>Digite um email:</label>
+    <input type="email" name="email" />
+    <button type="submit" name="btnSave">Salvar</button>
+</form>
--- database_connect/connect.php
+++ database_connect/connect.php
@@ -1,13 +1,35 @@
 <?php
-$pdo = new PDO("mysql:dbname=db_php;host=144.22.192.13", "root", "ag72qjh5");
-
+require 'config.php';
+$list = [];
 $sql = $pdo->query('SELECT * FROM users');
 
-$data = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-$total = $sql->rowCount();
 
-echo 'TOTAL DE REGISTROS: ' . $total;
+if ($sql->rowCount() > 0) {
+    $list = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
 
-echo '<pre>';
-print_r($data);
+?>
+<a href="addUser.php">Adicionar Usuário</a>
+<table border="1" width="100%">
+    <thead>
+        <th>ID</th>
+        <th>USUÁRIO</th>
+        <th>EMAIL</th>
+        <th>AÇÕES</th>
+    </thead>
+    <tbody>
+        <?php foreach ($list as $user) : ?>
+            <tr>
+                <td><?= $user['id']; ?></td>
+                <td><?= $user['name']; ?></td>
+                <td><?= $user['email']; ?></td>
+                <td align="center">
+                    <a href="edit.php?id=<?= $user['id']; ?>">[ Editar ]</a>
+                    <a href="delete.php?id=<?= $user['id']; ?>">[ Excluir ]</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+
+</table>
